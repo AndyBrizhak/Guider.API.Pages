0f3cea2f2ff9c7752042c6b2aab68ec1ef75710f
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-02-04 07:02:56 +0800

    Add services

--- Guider.API.Pages/Program.cs
+++ Guider.API.Pages/Program.cs
@@ -1,8 +1,13 @@
+using Guider.API.Pages.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSingleton<MongoDbContext>();
+builder.Services.AddScoped<GenericRepository>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
