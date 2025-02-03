22a9ced3d74f1e725b00be46a7b07c7611adc54c
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-02-04 06:19:13 +0800

    Add project files.

---
+++ Guider.API.Pages/Models/Page.cs
@@ -0,0 +1,14 @@
+﻿namespace Guider.API.Pages.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    public class Page
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("data")]
+        public BsonDocument Data { get; set; } = new BsonDocument();
+    }
+}
